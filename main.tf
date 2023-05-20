dc275b28e536ca9de7373e98854a1be5df3b0ce9
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-05-20 23:33:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,15 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "tjinst" {
-    ami = "ami-0b08bfc6ff7069aff"
-    instance_type = "t2.micro"
-    tags = {
-       Name = "TFJ-Instance"
+# resource "aws_instance" "tjinst" {
+#     ami = "ami-0b08bfc6ff7069aff"
+#     instance_type = "t2.micro"
+#     tags = {
+#        Name = "TFJ-Instance"
+#     }
+# }
+resource "null_resource" "test" {
+    provisioner "local-exec" {
+        command = "echo Hello World"
     }
 }
