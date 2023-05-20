1ec8542c26e64e7628bc692483b53e7022a21916
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2023-05-20 19:37:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "tjinst" {
+    ami = "ami-0b08bfc6ff7069aff"
+    instance_type = "t2.micro"
+    tags = {
+       Name = "TF-Instance"
+    }
+}
